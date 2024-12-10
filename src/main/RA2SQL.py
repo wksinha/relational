844d0c03d532646f845f9e589e7a8e716183fb3f
Warul K. Sinha <wksinha@outlook.com>
2024-12-10 14:08:49 +0530

    Implementation Complete

--- src/main/RA2SQL.py
+++ src/main/RA2SQL.py
@@ -1,3 +1,4 @@
+import sys
 from lark import Lark, Transformer, v_args
 
 LARK_TREE = "<class 'lark.tree.Tree'>"
@@ -6,14 +7,23 @@ START = "start"
 QUERY = "query"
 RELATION = "relation"
 SELECTION = "selection"
+PROJECTION = "projection"
+ATTRIBUTES = "attributes"
+UNION = "union"
+DIFFERENCE = "difference"
+PRODUCT = "product" # Cartesian Product
 CONDITION = "condition"
 WILDCARD = "*"
 
 dbg = []
-dbg.append(START)
-dbg.append(QUERY)
-dbg.append(SELECTION)
-dbg.append(CONDITION)
+# dbg.append(START)
+# dbg.append(QUERY)
+# dbg.append(SELECTION)
+# dbg.append(CONDITION)
+# dbg.append(PROJECTION)
+# dbg.append(UNION)
+# dbg.append(DIFFERENCE)
+# dbg.append(PRODUCT)
 
 '''
 Relational Algebra to SQL Converter
@@ -27,17 +37,21 @@ Supported Operations:
 '''
 
 ra_grammar = """
-start: query -> start
-query: relation | selection -> query
-relation: KEY -> relation
-selection: "SELECTION" "(" condition ")" "(" query ")" -> selection
-condition: WILDCARD | STRING [ (STRING)*] -> condition
-WILDCARD: "*"
-KEY: /[a-zA-Z0-9]+/
-STRING: /[a-zA-Z0-9>=<]+/
-%import common.WS
-%ignore WS
-
+    start: query -> start
+    query: relation | selection | projection | union | difference | product-> query
+    relation: KEY -> relation
+    selection: "SELECTION" "(" condition ")" "(" query ")" -> selection
+    projection: "PROJECTION" "(" attributes ")" "(" query ")" -> projection
+    union: "UNION" "(" query ")" "(" query")" -> union
+    difference: "DIFFERENCE" "(" query ")" "(" query")" -> difference
+    product: "PRODUCT" "(" query ")" "(" query ")" -> product
+    condition: WILDCARD | STRING [ (STRING)*] -> condition
+    attributes: STRING [("," STRING)*] -> attributes
+    WILDCARD: "*"
+    KEY: /[a-zA-Z0-9]+/
+    STRING: /[a-zA-Z0-9>=<]+/
+    %import common.WS
+    %ignore WS
 """
 
 
@@ -55,6 +69,7 @@ def resolve_tree(node):
                 print(node.data)
                 print(node.children)
                 print()
+
             expression = resolve_tree(node.children[0])
             return expression + ";"
         elif node.data == QUERY:
@@ -62,31 +77,78 @@ def resolve_tree(node):
                 print(node.data)
                 print(node.children)
                 print()
+
             expression = "(" + resolve_tree(node.children[0]) + ")"
         elif node.data == RELATION:
             if RELATION in dbg:
                 print(node.data)
                 print(node.children)
                 print()
+
             expression = resolve_tree(node.children[0])
         elif node.data == SELECTION:
             if SELECTION in dbg:
                 print(node.data)
                 print(node.children)
                 print()
+
             table = resolve_tree(node.children[1])
             expression = f"SELECT {WILDCARD} FROM {table}"
             conditions = resolve_tree(node.children[0])
             if conditions != WILDCARD:
                 expression += f" WHERE {conditions}"
-            
-        elif node.data == "condition":
+        elif node.data == CONDITION:
             if CONDITION in dbg:
                 print(node.data)
                 print(node.children)
                 print()
+
             conditions = [resolve_tree(child) for child in node.children]
-            return " ".join(conditions)
+            expression = " ".join(conditions)
+        elif node.data == PROJECTION:
+            if PROJECTION in dbg:
+                print(node.data)
+                print(node.children)
+                print()
+
+            attributes = resolve_tree(node.children[0])
+            table = resolve_tree(node.children[1])
+            expression = f"SELECT {attributes} FROM {table}"
+        elif node.data == ATTRIBUTES:
+            if ATTRIBUTES in dbg:
+                print(node.data)
+                print(node.children)
+                print()
+
+            attributes = [resolve_tree(child) for child in node.children]
+            expression = ", ".join(attributes)
+        elif node.data == UNION:
+            if UNION in dbg:
+                print(node.data)
+                print(node.children)
+                print()
+
+            left = resolve_tree(node.children[0])
+            right = resolve_tree(node.children[1])
+            expression = f"{left} UNION {right}"
+        elif node.data == DIFFERENCE:
+            if DIFFERENCE in dbg:
+                print(node.data)
+                print(node.children)
+                print()
+            
+            left = resolve_tree(node.children[0])
+            right = resolve_tree(node.children[1])
+            expression = f"{left} EXCEPT {right}"
+        elif node.data == PRODUCT:
+            if PRODUCT in dbg:
+                print(node.data)
+                print(node.children)
+                print()
+
+            left = resolve_tree(node.children[0])
+            right = resolve_tree(node.children[1])
+            expression = f"{left} CROSS JOIN {right}"
         else:
             raise SyntaxError('Invalid expression: %s' % node.data)
         
@@ -101,7 +163,21 @@ def cleanup(sqlquery):
     return sqlquery
 
 
-expr = input()
-sqlquery = resolve_tree(parse_ra_to_sql(expr))
-sqlquery = cleanup(sqlquery)
-print(sqlquery)
+def main():
+    try:
+        expr = input()
+    except IOError as e:
+        print(e, file=sys.stderr)
+        return        
+
+    try:
+        sqlquery = resolve_tree(parse_ra_to_sql(expr))
+        sqlquery = cleanup(sqlquery)
+    except Exception as e:
+        print("Failed to parse input for expression:", expr, ", error: ", e, file=sys.stderr)
+        return
+
+    print(sqlquery)
+
+if __name__=="__main__":
+    main()
--- src/main/examples.md
+++ src/main/examples.md
@@ -3,3 +3,13 @@
     - Wildcard selection
 2. SELECTION (year = 2) (SELECTION  (sid > 10) (student))
     - Nested selection
+3. PROJECTION (year, sid) (student)
+    - Projection
+4. PROJECTION (year) (SELECTION (year=1) (student))
+    - Projection over Selection
+5. UNION ( SELECTION (year=1) (student) ) ( SELECTION (year=2) (student) )
+    - Union
+6. DIFFERENCE (PROJECTION (sid) (SELECTION (*) (enrollment))) (PROJECTION (sid) (student))
+    - Difference
+7. PRODUCT (SELECTION (*) (student)) (SELECTION (*) (course))
+    - Cross Product
